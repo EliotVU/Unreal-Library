5c4cce07012d95de604fcf9f2a664a4faebcaa5d
EliotVU <eliot_van@hotmail.com>
2012-11-11 17:07:18 +0100

    Cleanup UnrealFlags.

--- UnrealFlags.cs
+++ UnrealFlags.cs
@@ -1,68 +1,3 @@
-/**
- * TODO:	
- * 	- Property Flags
- * 		---repretry(UE3+)
- * 		---notforconsole(UE3+)
- * 		---editoronly(UE3+)
- * 		---editconstarray(UE2+)
- * 		---editfixedsize(UE3+)
- * 		---databinding(UE3+)
- * 		---duplicatetransient(UE3+)
- * 		---edithide(UE3+)
- * 		---edfindable
- * 		---edittextbox(UE3+)
- * 		---interp(UE3+)
- * 		---noclear(UE3+)
- * 		---noimport(UE3+)
- * 		---nontransactional(UE3+)
- * 		---init(UE3)
- * 		---repnotify(UE3)
- * 		---crosslevelpassive(UE3+)
- * 		---protectedwrite(UE3+)
- * 		---privatewrite(UE3+)
- * 		---serializetext(UE3+)
- * 		---cache(ut2004)
- * 		---automated(ut2004)
- * 		---editinlinenotify(ue2)
- *		---export
- * 		
- * 	- Class Flags
- * 		---cacheexempt(ut2004)
- * 		---dependson(multi...)(not compiled)
- * 		---deprecated(UE3)
- * 		---implements(UE3)
- * 		---perobjectlocalized(UE3)
- * 		---transient,nontransient(UE3)
- * 		---autocollapsecategories(UE3)
- * 		---autoexpandcategories(UE3)
- * 		---hidedropdown(UE3)
- * 		---hidedropdown(UE2)
- * 		---dllbind(UE3)
- * 		***inherits(UE3)
- * 		---native(...)(UE3)
- * 		---exportstructs(UE2)
- * 		---instanced
- *		---parseconfig(UE2)
- * 		
- * 	- Struct Flags
- * 		---native(UE2)
- * 		---long(UE2)
- * 		---export(UE2)
- * 		---immutable(UE3)
- * 		---immutablewhencooked(UE3)
- * 		---atomic(UE3)
- * 		---atomicwhencooked(UE3)
- * 		---init
- * 		---configstrict(UE3)
- * 		---transient(UE3)?
- *
- *	- Function Flags
- *		---dllimport(UE3+)
- *		---server(UE3)
- *		---client(UE3)
- *		***noexportheader(UE3*)
- *
- */
 using System;
 
 namespace UELib.Flags
@@ -191,21 +126,21 @@ namespace UELib.Flags
 	public enum ObjectFlagsLO : ulong	// 32bit aligned, see ObjectFlags64
 	{
 		Transactional 		= 0x00000001U,	
-		Unreachable 		= 0x00000002U,	
+		//Unreachable 		= 0x00000002U,	
 		Public 				= 0x00000004U,	
-		TagImp 				= 0x00000008U,	
-		TagExp 				= 0x00000010U,	
-		SourceModified 		= 0x00000020U,		// @Redefined(UE3, Obsolete)
-		TagGarbage 			= 0x00000040U,	
+		//TagImp 				= 0x00000008U,	
+		//TagExp 				= 0x00000010U,	
+		//SourceModified 		= 0x00000020U,		// @Redefined(UE3, Obsolete)
+		//TagGarbage 			= 0x00000040U,	
 		Private 			= 0x00000080U,		// @Redefined(UE3, Final)
 		Automated 			= 0x00000100U,		// @Redefined(UE3, PerObjectLocalized)
-		NeedLoad 			= 0x00000200U,	
-		HighlightedName		= 0x00000400U,		// @Redefined(UE3, EliminateObject)
-		InSingularFunc	 	= 0x00000800U,		// @Redefined(UE3, RemappedName) 
-		Suppress			= 0x00001000U,		// @Redefined(UE3, StateChanged) 
-		InEndState			= 0x00002000U,		
+		//NeedLoad 			= 0x00000200U,	
+		//HighlightedName		= 0x00000400U,		// @Redefined(UE3, EliminateObject)
+		//InSingularFunc	 	= 0x00000800U,		// @Redefined(UE3, RemappedName) 
+		//Suppress			= 0x00001000U,		// @Redefined(UE3, StateChanged) 
+		//InEndState			= 0x00002000U,		
 		Transient			= 0x00004000U,		
-		Preloading			= 0x00008000U,		
+		//Preloading			= 0x00008000U,		
 		LoadForClient		= 0x00010000U,		
 		LoadForServer		= 0x00020000U,		
 		LoadForEdit			= 0x00040000U,		
@@ -213,43 +148,16 @@ namespace UELib.Flags
 		NotForClient		= 0x00100000U,		
 		NotForServer		= 0x00200000U,		
 		NotForEdit			= 0x00400000U,		
-		Destroyed			= 0x00800000U,		
-		NeedPostLoad		= 0x01000000U,		
+		//Destroyed			= 0x00800000U,		
+		//NeedPostLoad		= 0x01000000U,		
 		HasStack			= 0x02000000U,		
 		Native				= 0x04000000U,		
 		Marked				= 0x08000000U,		
 		//SWAT4_Unnamed		    = 0x08000000U,	
-		ErrorShutdown		= 0x10000000U,		
-		DebugPostLoad		= 0x20000000U,		// @Removed(UE3)
-		DebugSerialize		= 0x40000000U,		// @Removed(UE3)
-		DebugDestroy		= 0x80000000U,		// @Redefined(UE3, EdSelected)
-
-		#region Combinations
-		/// <summary>
-		/// // All context flags.
-		/// </summary>
-		ContextFlags		= NotForClient | NotForServer | NotForEdit,
-
-		/// <summary>
-		/// // Flags affecting loading.
-		/// </summary>
- 		LoadContextFlags	= LoadForClient | LoadForServer | LoadForEdit,
-
-		/// <summary>
-		/// // Flags to load from Unrealfiles.
-		/// </summary>
-		Load				= ContextFlags | LoadContextFlags | Public | ObjectFlagsHO.Final | Standalone | Native | ObjectFlagsHO.Obsolete | Transactional | HasStack | ObjectFlagsHO.PerObjectLocalized,
-		
-		/// <summary>
-		/// // Flags to persist across loads.
-		/// </summary>
-		Keep				= Native | Marked | ObjectFlagsHO.PerObjectLocalized,
- 		
-		/// <summary>
-		/// // Script-accessible flags.
-		/// </summary>
-		ScriptMask			= Transactional | Public | ObjectFlagsHO.Final | Transient | NotForClient | NotForServer | NotForEdit | Standalone
-		#endregion
+		//ErrorShutdown		= 0x10000000U,		
+		//DebugPostLoad		= 0x20000000U,		// @Removed(UE3)
+		//DebugSerialize		= 0x40000000U,		// @Removed(UE3)
+		//DebugDestroy		= 0x80000000U,		// @Redefined(UE3, EdSelected)
 	}
 
 	/// <summary>
@@ -310,13 +218,7 @@ namespace UELib.Flags
 		NetClient			= 0x01000000U,
 		DLLImport			= 0x02000000U,		// Also available in UE2(unknown meaning there)
 		K2Pure 				= 0x04000000U,
-		Unk4 				= 0x08000000U,
-
-		#region Combinations
-		FuncInherit        	= Exec | Event,
-		FuncOverrideMatch	= Exec | Final | Latent | PreOperator | Iterator | Static | Public | Protected,
-		NetFuncFlags       	= Net | NetReliable
-		#endregion
+		Unk4 				= 0x08000000U,		// K2Call?
 	}
 
 	/// <summary>
@@ -485,7 +387,7 @@ namespace UELib.Flags
 		Parsed 				= 0x00000010U,
 		Localized 			= 0x00000020U,
 		SafeReplace 		= 0x00000040U,
-		RuntimeStatic 		= 0x00000080U,
+		//RuntimeStatic 		= 0x00000080U,
 		NoExport 			= 0x00000100U,
 		Placeable 			= 0x00000200U,
 		PerObjectConfig 	= 0x00000400U,
@@ -493,12 +395,12 @@ namespace UELib.Flags
 		EditInlineNew 		= 0x00001000U,
 		CollapseCategories 	= 0x00002000U,
 		ExportStructs 		= 0x00004000U,		// @Removed(UE3 in early but not latest)
-		IsAUProperty      	= 0x00008000U,	
-    	IsAUObjectProperty	= 0x00010000U,	
-    	IsAUBoolProperty  	= 0x00020000U,	
-    	IsAUState         	= 0x00040000U,	
-    	IsAUFunction      	= 0x00080000U,	
-		NeedsDefProps		= 0x00100000U,	
+		//IsAUProperty      	= 0x00008000U,	
+    	//IsAUObjectProperty	= 0x00010000U,	
+    	//IsAUBoolProperty  	= 0x00020000U,	
+    	//IsAUState         	= 0x00040000U,	
+    	//IsAUFunction      	= 0x00080000U,	
+		//NeedsDefProps		= 0x00100000U,	
 		Instanced			= 0x00200000U,		// @Removed(UE3)
 		HideDropDown		= 0x00400000U,		// @Redefined(UE3, HasComponents), @Moved(UE3, HideDropDown2)
 		ParseConfig			= 0x01000000U,		// @Redefined(UE3, Deprecated)
@@ -515,17 +417,6 @@ namespace UELib.Flags
 		Exported			= 0x04000000U,		
 		NativeOnly			= 0x20000000U,
   		#endregion
-
-		#region Combinations
-		// Flags to inherit from base class.
-		Inherit           	= Transient | Config | Localized | SafeReplace | 
-								RuntimeStatic | PerObjectConfig | Placeable | 
-								IsAUProperty | IsAUObjectProperty | IsAUBoolProperty |
-								IsAUState | IsAUFunction | HasComponents | Deprecated,
-
-		RecompilerClear   	= Inherit | Abstract | NoExport | NativeReplication,
-		ScriptInherit		= Inherit | EditInlineNew | CollapseCategories
-		#endregion
 	}
 
 	/// <summary>
