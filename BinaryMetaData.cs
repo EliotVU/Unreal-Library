4fd90656fd5a765610a7d1c992708e105eb6c03f
EliotVU <eliot_van@hotmail.com>
2013-01-07 00:31:24 +0100

    [Fix]: Dutch localization for Hex-Viewer's "View" items. [Fix]: NTL generator not rendering run-time updates. [Rev]: TabsManager.
    Updated license.txt

--- BinaryMetaData.cs
+++ BinaryMetaData.cs
@@ -2,41 +2,41 @@
 
 namespace UELib
 {
-	public class BinaryMetaData
-	{
-		public struct BinaryField : IUnrealDecompilable
-		{
-			public string Name;
-			public object Tag;
-			public long Position;
-			public long Size;
+    public class BinaryMetaData
+    {
+        public struct BinaryField : IUnrealDecompilable
+        {
+            public string Name;
+            public object Tag;
+            public long Position;
+            public long Size;
 
-			public string Decompile()
-			{
-				return Tag != null ? Tag.ToString() : "NULL";
-			}
-		}
+            public string Decompile()
+            {
+                return Tag != null ? Tag.ToString() : "NULL";
+            }
+        }
 
-		public readonly IList<BinaryField> Fields;
+        public Stack<BinaryField> Fields;
 
-		public BinaryMetaData()
-		{
-			Fields = new List<BinaryField>();	
-		}
+        [System.Diagnostics.Conditional( "DEBUG" )]
+        public void AddField( string name, object tag, long position, long size )
+        {
+            if( Fields == null )
+            {
+                Fields = new Stack<BinaryField>();
+            }
 
-		[System.Diagnostics.Conditional( "DEBUG" )]
-		public void AddField( string name, object tag, long position, long size )
-		{
-			Fields.Add
-			( 
-				new BinaryField
-				{
-					Name = name,
-					Tag = tag,
-					Position = position,
-					Size = size
-				}
-			);
-		}
-	}
+            Fields.Push
+            ( 
+                new BinaryField
+                {
+                    Name = name,
+                    Tag = tag,
+                    Position = position,
+                    Size = size
+                }
+            );
+        }
+    }
 }
