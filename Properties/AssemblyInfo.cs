7785c77f9ede08e65589da8d178f72bc19f339e4
EliotVU <eliot_van@hotmail.com>
2013-12-01 21:23:15 +0100

    Added translation for "View Disassembled Tokens". Bump version to 1.2.5.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "UELib" )]
-[assembly: AssemblyDescription( "UELib makes it possible to deserialize and decompile almost any Unreal package files." )]
+[assembly: AssemblyDescription( "A library for parsing Unreal Engine packages." )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany( "EliotVU" )]
 [assembly: AssemblyProduct( "UELib" )]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.2.4.1" )]
-[assembly: AssemblyFileVersion( "1.2.4.1" )]
+[assembly: AssemblyVersion( "1.2.5.0" )]
+[assembly: AssemblyFileVersion( "1.2.5.0" )]
