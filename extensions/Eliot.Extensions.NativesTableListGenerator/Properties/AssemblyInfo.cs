d68a8607ee3bdaef21c2d76d298a2cf34e8c71ed
EliotVU <eliot_van@hotmail.com>
2013-12-19 21:58:23 +0100

    Added an example of an UE Explorer extension that uses the library and the program to bind itself to UE Explorer's extensions dropdown.

---
+++ extensions/Eliot.Extensions.NativesTableListGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "UE Extensions" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "UE Extensions" )]
+[assembly: AssemblyCopyright( "Copyright 2012 Eliot van Uytfanghe." )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "e4c70463-84c0-4851-ba33-7c1f17ade5bf" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ extensions/Eliot.Extensions.NativesTableListGenerator/UC_NativeGenerator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using UEExplorer.UI.Tabs;
+using UELib;
+using UELib.Core;
+using UELib.Flags;
+using UEExplorer.Development;
+using UEExplorer.UI;
+
+namespace Eliot.Extensions.NativesTableListGenerator
+{
+    [System.Runtime.InteropServices.ComVisible( false )]
+    public partial class UC_NativeGenerator : UserControl_Tab
+    {
+        private readonly NativesTablePackage _NTLPackage = new NativesTablePackage();
+
+        public UC_NativeGenerator()
+        {
+            _NTLPackage.NativeTableList = new List<NativeTableItem>();
+        }
+
+        private void Button_Add_Click( object sender, EventArgs e )
+        {
+            var dialogResult = OpenNTLDialog.ShowDialog( this );
+            if( dialogResult != DialogResult.OK )
+            {
+                return;
+            }
+
+            var packages = new Stack<UnrealPackage>();
+            foreach( var fileName in OpenNTLDialog.FileNames )
+            {
+                packages.Push( UnrealLoader.LoadPackage( fileName ) );
+            }
+
+            if( packages.Count > 0 )
+            {
+                FileNameTextBox.Enabled = true;
+                Button_Save.Enabled = true;
+            }
+    
+            TreeView_Packages.BeginUpdate();
+            foreach( var package in packages )
+            {
+                package.RegisterClass( "Function", typeof(UFunction) );
+                package.InitializeExportObjects( UnrealPackage.InitFlags.Deserialize );
+
+                var packageNode = TreeView_Packages.Nodes.Add( package.PackageName );
+                foreach( var function in package.Objects.OfType<UFunction>() )
+                {
+                    if( !function.HasFunctionFlag( FunctionFlags.Native ) || function.NativeToken == 0 ) 
+                        continue;
+
+                    var item = new NativeTableItem( function );
+                    var itemNode = packageNode.Nodes.Add( item.Name );
+                    itemNode.Nodes.Add( "Format:" + item.Type );
+                    itemNode.Nodes.Add( "ByteToken:" + item.ByteToken );
+                    itemNode.Nodes.Add( "OperPrecedence:" + item.OperPrecedence );
+
+                    _NTLPackage.NativeTableList.Add( item );
+                }
+                TreeView_Packages.Refresh();
+                package.Stream.Close();
+            }
+            TreeView_Packages.EndUpdate();
+        }
+
+        private void Button_Save_Click( object sender, EventArgs e )
+        {
+            _NTLPackage.CreatePackage
+            ( 
+                Path.Combine
+                ( 
+                    Application.StartupPath, 
+                    "Native Tables", 
+                    "NativesTableList_" + FileNameTextBox.Text 
+                ) 
+            );
+        }
+    } 
+
+    [ExtensionTitle( "NTL Generator" )]
+    public class ExtNativeGen : IExtension
+    {
+        private ProgramForm _Form;
+
+        /// <summary>
+        /// Called after UEExplorer_Form is initialized.
+        /// </summary>
+        /// <param name="form"></param>
+        public void Initialize( ProgramForm form )
+        {
+            _Form = form;
+        }
+
+        /// <summary>
+        /// Called when activated by end-user.
+        /// </summary>
+        public void OnActivate( object sender, EventArgs e )
+        {
+            _Form.Tabs.Add( typeof(UC_NativeGenerator), "Natives Table List Generator" );
+        }
+    }
+}
