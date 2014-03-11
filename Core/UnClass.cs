bd7e64f97ed17ae5474736d80fedd20e856bfa03
EliotVU <eliot_van@hotmail.com>
2014-03-11 15:11:37 +0100

    Deserialization fix for UDK version 674.

--- Core/UnClass.cs
+++ Core/UnClass.cs
@@ -244,7 +244,7 @@ namespace UELib.Core
 
                             // FIXME: Found first in(V:655), Definitely not in APB and GoW 2
                             // TODO: Corrigate Version
-                            if( Package.Version > 575 && Package.Version < 678 )
+                            if( Package.Version > 575 && Package.Version < 674 )
                             {
                                 int unk2 = _Buffer.ReadInt32();
                                 Record( "??Int32", unk2 );
