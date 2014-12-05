cb11da552ec2d6b00e880d1410763daad6dad401
Eliot van Uytfanghe <eliot_van@hotmail.com>
2014-12-05 06:42:49 +0100

    Move UArray and UName to its own folder /Types/.

--- src/UnrealTables.cs
+++ src/UnrealTables.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.IO;
 using UELib.Core;
@@ -72,104 +71,6 @@ namespace UELib
                 : base.ToString();
         }
         #endregion
-    }
-
-    public sealed class UName : IUnrealSerializableClass
-    {
-        private const string    None = "None";
-        public const int        Numeric = 0;
-        internal const int      VNameNumbered = 343;
-
-        private UNameTableItem  _NameItem;
-        private int             _Number;
-        public int              Number{ get; private set; }
-
-        private string          _Text
-        {
-            get{ return _Number > Numeric ? _NameItem.Name + "_" + _Number : _NameItem.Name; }
-        }
-
-        private int             _Index
-        {
-            get{ return _NameItem.Index; }
-        }
-
-        public int              Index{ get; private set; }
-
-        public int              Length
-        {
-            get{ return _Text.Length; }
-        }
-
-        public UName( IUnrealStream stream )
-        {
-            Deserialize( stream );
-        }
-
-        public UName( UNameTableItem nameItem, int num )
-        {
-            _NameItem = nameItem;
-            _Number = num;
-        }
-
-        public bool IsNone()
-        {
-            return _NameItem.Name.Equals( None, StringComparison.OrdinalIgnoreCase );
-        }
-
-        public void Deserialize( IUnrealStream stream )
-        {
-            var index = stream.ReadNameIndex( out _Number );
-            _NameItem = stream.Package.Names[index];
-
-            Debug.Assert( _NameItem != null, "_NameItem cannot be null! " + index );
-            Debug.Assert( _Number >= -1, "Invalid _Number value! " + _Number );
-        }
-
-        public void Serialize( IUnrealStream stream )
-        {
-            stream.WriteIndex( _Index );
-            if( stream.Version >= VNameNumbered )
-            {
-                Console.WriteLine( _Number + " " + _Text );
-                stream.Write( (uint)_Number + 1 );
-            }
-        }
-
-        public override string ToString()
-        {
-            return _Text;
-        }
-
-        public static bool operator ==( UName a, object b )
-        {
-            return Equals( a, b );
-        }
-
-        public static bool operator !=( UName a, object b )
-        {
-            return !Equals( a, b );
-        }
-
-        public static bool operator ==( UName a, string b )
-        {
-            return String.Equals( a, b );
-        }
-
-        public static bool operator !=( UName a, string b )
-        {
-            return !String.Equals( a, b );
-        }
-
-        public static implicit operator string( UName a )
-        {
-            return !Equals( a, null ) ? a._Text : null;
-        }
-
-        public static explicit operator int( UName a )
-        {
-            return a._Index;
-        }
     }
 
     /// <summary>
