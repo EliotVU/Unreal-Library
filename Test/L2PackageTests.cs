d9937d04d8f8e56989cc617c63b9253550dbf2d3
notpaimon <katerinapaimon@hotmail.com>
2021-02-24 06:14:35 +0300

    Json serialization add awful crutches to make it serialize into json rather than to unparseable text mess

--- Test/L2PackageTests.cs
+++ Test/L2PackageTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace UELib.Test
@@ -19,23 +18,26 @@ namespace UELib.Test
         [TestCase(MAP_PACKAGE_PATH, "LevelInfo")]
         [TestCase(MAP_PACKAGE_PATH, "AmbientSoundObject")]
         [TestCase(MAP_PACKAGE_PATH, "Emitter")]
+        [TestCase(MAP_PACKAGE_PATH, "PhysicsVolume")]
         public void LoadMapPackage_DecompileObjects_ShouldBeValidJson(string mapPath, string objectTypeName)
         {
             var package = UnrealLoader.LoadFullPackage(mapPath);
 
             for (var i = 0; i < package.Objects.Count; i++)
             {
-                if (!package.Objects[i].Name.StartsWith(objectTypeName))
+                var obj = package.Objects[i];
+                
+                // obj.Name.Length > objectTypeName.Length is to include only instances, not bare class types
+                if (!obj.Name.StartsWith(objectTypeName) && obj.Name.Length > objectTypeName.Length)
                     continue;
                 
-                var json = package.Objects[i].Decompile();
-                
-                TestContext.Out.Write(json);
-                
-                Assert.False(string.IsNullOrEmpty(json));
-
                 try
                 {
+                    var json = obj.Decompile();
+                
+                    TestContext.Out.Write(json);
+                
+                    Assert.False(string.IsNullOrEmpty(json));
                     Assert.IsTrue(JsonConvert.DeserializeObject(json) != null);
                 }
                 catch (Exception e)
