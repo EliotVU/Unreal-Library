b7becc2f62ea1fc781e61a807aa12387b463eb19
Eliot <contact@eliotvu.com>
2022-04-22 19:30:54 +0200

    CLI updates

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace UELib.CLI
 {
     public static class Program
@@ -7,7 +9,7 @@ namespace UELib.CLI
         /// Basic command line app for UELib.dll.
         /// 
         /// The CLI parses the commandline as following:
-        /// <code>["path to file"] [command]</code>
+        /// <code>["path to file"] ["command"]</code>
         /// For example: <code>"Core.u" "obj decompile Object"</code>
         /// Command format: <code>[command] [action] [object path]</code>
         /// Supported commands:
@@ -46,62 +48,92 @@ namespace UELib.CLI
                     string actionName = cmdArgs.Length > 1 ? cmdArgs[1] : "unspecified";
                     switch (actionName.ToLowerInvariant())
                     {
-                        case "get":
+                        case "index":
+                        {
+                            string parm = cmdArgs.Length > 2 ? cmdArgs[2] : null;
+                            int index = int.Parse(parm);
+                            var obj = pkg.GetIndexObject(index);
+                            if (obj == null)
                             {
-                                string objectPath = cmdArgs.Length > 2 ? cmdArgs[2] : null;
-                                if (objectPath == null)
-                                {
-                                    Console.Error.WriteLine("Missing object path");
-                                    break;
-                                }
-
-                                var obj = pkg.FindObjectByGroup(objectPath);
-                                if (obj == null)
-                                {
-                                    Console.Error.WriteLine("Couldn't find object by path '{0}'", objectPath);
-                                    break;
-                                }
-
-                                string propertyName = cmdArgs.Length > 3 ? cmdArgs[3] : null;
-                                if (propertyName == null)
-                                {
-                                    Console.Error.WriteLine("Missing property name");
-                                    break;
-                                }
-
-
-                                // Hack: Temporary workaround the limitations of UELib.
-                                obj.BeginDeserializing();
-                                var defaultProperty = obj.Properties?.Find(propertyName);
-                                if (defaultProperty == null)
-                                {
-                                    Console.Error.WriteLine("Object '{0}' has no property of the name '{1}'",
-                                        obj.GetOuterGroup(), propertyName);
-                                    break;
-                                }
-
-                                string output = defaultProperty.Decompile();
-                                int letIndex = output.IndexOf('=');
-                                if (letIndex != -1) output = output.Substring(output.IndexOf('=') + 1);
-                                Console.WriteLine(output);
+                                Console.Error.WriteLine("Invalid index");
                                 break;
                             }
+
+                            Console.WriteLine($"Object:{obj.GetOuterGroup()}");
+                            break;
+                        }
+                        
+                        case "list":
+                        {
+                            string classLimitor = cmdArgs.Length > 2 ? cmdArgs[2] : null;
+                            if (classLimitor == null)
+                            {
+                                foreach (var obj in pkg.Objects)
+                                    Console.WriteLine($"{obj.GetClassName()}'{obj.GetOuterGroup()}'");
+                                break;
+                            }
+
+                            foreach (var obj in pkg.Objects.Where(e => e.Class?.Name == classLimitor))
+                                Console.WriteLine($"{obj.GetClassName()}'{obj.GetOuterGroup()}'");
+                            break;
+                        }
+
+                        case "get":
+                        {
+                            string objectPath = cmdArgs.Length > 2 ? cmdArgs[2] : null;
+                            if (objectPath == null)
+                            {
+                                Console.Error.WriteLine("Missing object path");
+                                break;
+                            }
+
+                            var obj = pkg.FindObjectByGroup(objectPath);
+                            if (obj == null)
+                            {
+                                Console.Error.WriteLine("Couldn't find object by path '{0}'", objectPath);
+                                break;
+                            }
+
+                            string propertyName = cmdArgs.Length > 3 ? cmdArgs[3] : null;
+                            if (propertyName == null)
+                            {
+                                Console.Error.WriteLine("Missing property name");
+                                break;
+                            }
+
+
+                            // Hack: Temporary workaround the limitations of UELib.
+                            obj.BeginDeserializing();
+                            var defaultProperty = obj.Properties?.Find(propertyName);
+                            if (defaultProperty == null)
+                            {
+                                Console.Error.WriteLine("Object '{0}' has no property of the name '{1}'",
+                                    obj.GetOuterGroup(), propertyName);
+                                break;
+                            }
+
+                            string output = defaultProperty.Decompile();
+                            int letIndex = output.IndexOf('=');
+                            if (letIndex != -1) output = output.Substring(output.IndexOf('=') + 1);
+                            Console.WriteLine(output);
+                            break;
+                        }
 
                         case "decompile":
+                        {
+                            string objectPath = cmdArgs.Length > 2 ? cmdArgs[2] : "unspecified";
+                            var obj = pkg.FindObjectByGroup(objectPath);
+                            if (obj == null)
                             {
-                                string objectPath = cmdArgs.Length > 2 ? cmdArgs[2] : "unspecified";
-                                var obj = pkg.FindObjectByGroup(objectPath);
-                                if (obj == null)
-                                {
-                                    Console.Error.WriteLine("Couldn't find object by path '{0}'", objectPath);
-                                    break;
-                                }
-
-                                obj.BeginDeserializing();
-                                string output = obj.Decompile();
-                                Console.WriteLine(output);
+                                Console.Error.WriteLine("Couldn't find object by path '{0}'", objectPath);
                                 break;
                             }
+
+                            obj.BeginDeserializing();
+                            string output = obj.Decompile();
+                            Console.WriteLine(output);
+                            break;
+                        }
 
                         default:
                             Console.Error.WriteLine("Unrecognized action '{0}'", actionName);
