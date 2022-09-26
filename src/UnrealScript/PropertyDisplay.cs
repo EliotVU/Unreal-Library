4ac6a7063e24c8a80ad59e080dc40e711ec614f7
Eliot <contact@eliotvu.com>
2022-09-26 20:49:13 +0200

    #52 This'll do for now.

--- src/UnrealScript/PropertyDisplay.cs
+++ src/UnrealScript/PropertyDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Text;
+using UELib.Core;
 using UELib.Core.Types;
 
 namespace UELib.UnrealScript
@@ -10,8 +11,6 @@ namespace UELib.UnrealScript
         /// <summary>
         /// Recodes escaped characters
         /// https://stackoverflow.com/a/14087738/617087
-        ///
-        /// FIXME: Escape \n in UE3
         /// </summary>
         public static string FormatLiteral(string input)
         {
@@ -23,6 +22,8 @@ namespace UELib.UnrealScript
                 {
                     case '\"': literal.Append("\\\""); break;
                     case '\\': literal.Append(@"\\"); break;
+                    case '\n': literal.Append(@"\n"); break;
+                    case '\r': literal.Append(@"\r"); break;
                     default: literal.Append(c); break;
                 }
             }
@@ -43,23 +44,55 @@ namespace UELib.UnrealScript
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatLiteral(short input)
+        {
+            return input.ToString("D", CultureInfo.InvariantCulture);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatLiteral(ushort input)
+        {
+            return input.ToString("D", CultureInfo.InvariantCulture);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string FormatLiteral(int input)
         {
-            return input.ToString(CultureInfo.InvariantCulture);
+            return input.ToString("D", CultureInfo.InvariantCulture);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string FormatLiteral(long input)
+        public static string FormatLiteral(uint input)
         {
-            return input.ToString(CultureInfo.InvariantCulture);
+            return input.ToString("D", CultureInfo.InvariantCulture);
         }
-
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string FormatLiteral(float input)
         {
-            return input.ToString(CultureInfo.InvariantCulture);
+            return input.ToString("F7", CultureInfo.InvariantCulture);
         }
-
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatLiteral(long input)
+        {
+            return input.ToString("F15", CultureInfo.InvariantCulture);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatLiteral(ulong input)
+        {
+            return input.ToString("F15", CultureInfo.InvariantCulture);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatLiteral(UObject input)
+        {
+            return input != null
+                ? $"{input.Class?.Name}'{input.GetOuterGroup()}'"
+                : "none";
+        }
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string FormatLiteral(UColor input)
         {
@@ -71,6 +104,18 @@ namespace UELib.UnrealScript
         public static string FormatStructLiteral(string args)
         {
             return $"({args})";
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatExport(float input)
+        {
+            return input.ToString("+00000.000000;-00000.000000", CultureInfo.InvariantCulture);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatExport(UVector input)
+        {
+            return $"{FormatExport(input.X)},{FormatExport(input.Y)},{FormatExport(input.Z)}";
         }
     }
 }
