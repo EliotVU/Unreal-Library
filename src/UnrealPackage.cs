de3cccec6ee05bfb13abed967eb1e7c3f005017d
Eliot <contact@eliotvu.com>
2024-04-07 16:46:04 +0200

    Update UnrealPackage.cs

--- src/UnrealPackage.cs
+++ src/UnrealPackage.cs
@@ -587,6 +587,15 @@ namespace UELib
                 [Build(874, 78u)] Battleborn,
 
                 /// <summary>
+                /// A Hat in Time
+                /// 
+                /// 877:893/005
+                /// 
+                /// The earliest available version with any custom specifiers is 1.0 (877) - Un-Drew.
+                /// </summary>
+                [Build(877, 893, 5, 5)] AHIT,
+
+                /// <summary>
                 /// Special Force 2
                 /// 
                 /// 904/009 (Non-standard version, actual Epic version might be 692 or higher)
@@ -594,15 +603,6 @@ namespace UELib
                 [Build(904, 904, 09u, 014u)]
                 [OverridePackageVersion((uint)PackageObjectLegacyVersion.ProbeMaskReducedAndIgnoreMaskRemoved)]
                 SpecialForce2,
-
-                /// <summary>
-                /// A Hat in Time
-                /// 
-                /// 877:893/005
-                /// 
-                /// The earliest available version with any custom specifiers is 1.0 (877).
-                /// </summary>
-                [Build(877, 893, 5, 5)] AHIT,
             }
 
             public BuildName Name { get; }
