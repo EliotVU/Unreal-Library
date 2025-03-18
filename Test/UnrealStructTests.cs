3aa75208355c2113be3c8446fbf6f4f180bb65e4
Eliot <contact@eliotvu.com>
2025-03-18 20:26:53 +0100

    Fixup marshalling of UMatrix type (inline the floats instead of having a  "fixed" array)
    (cherry picked from commit a9da6b7d7d8532bf13bd9d4486167695a4e0b8b3)

---
+++ Test/UnrealStructTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UELib.Core;
+
+namespace Eliot.UELib.Test
+{
+    [TestClass]
+    public class UnrealStructTests
+    {
+        [TestMethod]
+        public void TestUnrealStructs()
+        {
+            // Validate non padded sizes.
+            Assert.AreEqual(25, Unsafe.SizeOf<UBox>());
+            Assert.AreEqual(17, Unsafe.SizeOf<UScale>());
+            Assert.AreEqual(64, Unsafe.SizeOf<UMatrix>());
+
+            // Validate casting
+            var identity = (UMatrix)Matrix4x4.Identity;
+            var intrinsicMatrix = (Matrix4x4)identity;
+            Assert.AreEqual(Matrix4x4.Identity, intrinsicMatrix);
+        }
+    }
+}
