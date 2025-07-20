659e5c2c6b6dfecdbcf7a0385ee1f868650ab58c
Eliot <contact@eliotvu.com>
2025-07-21 00:57:09 +0200

    Add the "UnrealProperty" attribute; this attribute is to used to auto-bind property fields to the tagged property if any (implemented in another branch)

---
+++ src/ObjectModel/Annotations/UnrealPropertyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UELib.ObjectModel.Annotations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class UnrealPropertyAttribute : Attribute
+{
+    public readonly string Name;
+
+    public UnrealPropertyAttribute()
+    {
+    }
+
+    public UnrealPropertyAttribute(string name)
+    {
+        Name = name;
+    }
+}
