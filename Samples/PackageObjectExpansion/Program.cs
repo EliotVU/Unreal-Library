995fb8b989970aaf136a5656084fa9f70166e9b5
Eliot <contact@eliotvu.com>
2025-08-08 11:35:54 +0200

    Fix the ObjectExpansion sample to use the new streams.

--- Samples/PackageObjectExpansion/Program.cs
+++ Samples/PackageObjectExpansion/Program.cs
@@ -5,17 +5,18 @@ using System.Diagnostics;
 using UELib;
 
 // Has to be a decompressed(and decrypted if any) package!
-var packagePath = @"Assets/TestUC3.u";
+const string packagePath = "Assets/TestUC3.u";
 
-using var stream = new UPackageStream(packagePath, FileMode.Open, FileAccess.ReadWrite);
-using var package = new UnrealPackage(stream);
+using var fileStream = new FileStream(packagePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+using var package = new UnrealPackage(fileStream);
+var stream = package.Stream;
 package.BuildTarget = UnrealPackage.GameBuild.BuildName.Unset;
 package.Deserialize(stream);
 
 // In order for FindObjectByGroup to work, we need to initialize the export objects.
 package.InitializePackage(UnrealPackage.InitFlags.RegisterClasses | UnrealPackage.InitFlags.Construct);
 
-var objectToExpandName = "UC3Test.Test"; // ClassName.FunctionName
+const string objectToExpandName = "UC3Test.Test"; // ClassName.FunctionName
 var testFunction = package.FindObjectByGroup(objectToExpandName);
 Console.WriteLine(testFunction == null
     ? $"Object '{objectToExpandName}' not found in package."
@@ -23,11 +24,11 @@ Console.WriteLine(testFunction == null
 
 if (testFunction != null)
 {
-    var export = testFunction.ExportTable;
+    var export = testFunction.ExportResource;
     Debug.Assert(export != null, "Object must be an export");
 
     int newSerialSize = export.SerialSize + 100; // Increase size by 100 bytes
-    var newBuffer = new byte[newSerialSize];
+    byte[] newBuffer = new byte[newSerialSize];
 
     stream.Seek(export.SerialOffset, SeekOrigin.Begin);
     int copy = stream.Read(newBuffer, 0, export.SerialSize); // read the old data
@@ -41,6 +42,7 @@ if (testFunction != null)
     // Write the new serial offset and size to the export
     stream.Seek(export.Offset, SeekOrigin.Begin); // Offset to the export item.
     export.Serialize(stream); // Because the export item size itself hasn't changed, we can cheat and serialize it in place
-
+    Debug.Assert(export.Offset + export.Size == (int)stream.Position, "Export size cannot change.");
+    
     stream.Flush(); // Ensure all changes are written to the file
 }
