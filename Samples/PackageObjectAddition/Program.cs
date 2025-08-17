f1ac1d39bcf1faf6d11804e1b19b4b7f502837c5
Eliot <contact@eliotvu.com>
2025-08-17 21:30:48 +0200

    Add a basic builder for UObject classes; and a sample using it to build a new function AND saving this new function and class modifications back to the .u file.

---
+++ Samples/PackageObjectAddition/Program.cs
@@ -0,0 +1,96 @@
+﻿// Adds a new function to an existing class, by appending it to the end of the package file.
+
+using System.Diagnostics;
+using UELib;
+using UELib.Core;
+using UELib.Flags;
+using static UELib.Core.UStruct.UByteCodeDecompiler;
+
+// Has to be a decompressed(and decrypted if any) package!
+const string packagePath = "Assets/TestUC3.u";
+
+using var fileStream = new FileStream(packagePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+using var package = new UnrealPackage(fileStream);
+
+var packageStream = package.Stream;
+package.BuildTarget = UnrealPackage.GameBuild.BuildName.Unset;
+package.Deserialize(packageStream);
+
+// In order for FindObjectByGroup to work, we need to initialize the export objects.
+package.InitializePackage(UnrealPackage.InitFlags.RegisterClasses | UnrealPackage.InitFlags.Construct);
+
+const string objectTarget = "UC3Test";
+var objectResult = package.FindObjectByGroup(objectTarget);
+Console.WriteLine(objectResult == null
+    ? $"Object '{objectTarget}' not found in package."
+    : $"Found object: {objectResult.Name} of type {objectResult.Class?.Name ?? "Class"}");
+
+if (objectResult is UClass targetClass)
+{
+    // Load the fields, so we can modify them.
+    targetClass.Load();
+
+    var export = targetClass.ExportResource;
+    Debug.Assert(export != null, "Object must be an export");
+
+    var newFunctionName = new UName("MyNewAddedFunctionName");
+    var tokenFactory = package.Branch.GetTokenFactory(package);
+    // Let's call a function, recursively!
+    var funcCall = new VirtualFunctionToken
+    {
+        OpCode = tokenFactory.GetOpCodeFromTokenType<VirtualFunctionToken>(),
+        FunctionName = newFunctionName,
+        Arguments = [tokenFactory.CreateToken<EndFunctionParmsToken>()]
+    };
+
+    var returnToken = tokenFactory.CreateToken<ReturnToken>();
+    returnToken.ReturnExpression = tokenFactory.CreateToken<NothingToken>();
+    var endOfScript = tokenFactory.CreateToken<EndOfScriptToken>();
+
+    UnrealPackageBuilder
+        .Operate(package)
+        .AddName(newFunctionName)
+        .AddResource(UnrealObjectBuilder
+                     .CreateFunction(package)
+                     .FriendlyName(newFunctionName)
+                     .FunctionFlags(FunctionFlag.Public, FunctionFlag.Defined)
+                     .Outer(targetClass) // We need to set this before the call to AddResource finalizes.
+                     .WithStatements(funcCall, returnToken, endOfScript)
+                     .Build(out var newFunction));
+
+    // Register the new function in the target class.
+    var newClass = UnrealObjectBuilder
+                   .Operate(targetClass)
+                   .AddField(newFunction)
+                   .Build();
+
+    // Ensure that the internal UnrealName.None can be mapped back to the existing 'None' index in the current package.
+    package.Archive.NameIndices[UnrealName.None.Index] = package.Names.FindIndex(name => ((IndexName)name).Index == UnrealName.None.Index);
+    package.Summary.NameOffset = (int)packageStream.Length; // Write out the name table and other tables to the end of the stream.
+    packageStream.Seek(0, SeekOrigin.Begin);
+    package.Serialize(packageStream);
+
+    // Re-write the updated class, hopefully the class hasn't changed in size. (for UE2 and older this may cause change in size)
+    packageStream.Seek(newClass.ExportResource!.SerialOffset, SeekOrigin.Begin);
+    newClass.Save(packageStream);
+
+    // Write out the new function to the end of the stream.
+    packageStream.Seek(0, SeekOrigin.End);
+    int serialOffset = (int)packageStream.Position;
+    newFunction.Save(packageStream); // save the new function at the end of the stream
+    newFunction.ExportResource!.SerialOffset = serialOffset;
+    newFunction.ExportResource!.SerialSize = (int)packageStream.Position - serialOffset;
+
+    // Move the entire export table to the end of the stream (to make room for the new function)
+    package.Summary.ExportOffset = (int)packageStream.Length;
+
+    packageStream.Seek(0, SeekOrigin.Begin);
+    package.Serialize(packageStream); // also writes out all the other tables and dependencies table.
+
+    packageStream.Flush(); // Ensure all changes are written to the file
+
+    // sanity test
+    //package.InitializePackage();
+
+    Debug.Assert(package.FindObjectByGroup(newFunctionName) != null, "New function was not added successfully.");
+}
---
+++ src/UnrealObjectBuilder.cs
@@ -0,0 +1,467 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using UELib.Core;
+using UELib.Flags;
+
+namespace UELib;
+
+public static class UnrealPackageBuilder
+{
+    public static PackageBuilder Create(string packageName)
+    {
+        var package = new UnrealPackage(packageName);
+
+        return new PackageBuilder(package);
+    }
+
+    public static PackageBuilder Operate(UnrealPackage package)
+    {
+        return new PackageBuilder(package);
+    }
+
+    public static PackageBuilder Builder(this UnrealPackage package)
+    {
+        return Operate(package);
+    }
+}
+
+// VERY basic builder for UnrealPackage.
+// We'll add to it as the samples develop.
+public sealed class PackageBuilder(UnrealPackage package)
+{
+    private readonly UnrealPackage _Linker = package;
+
+    public UnrealPackage Get()
+    {
+        return package;
+    }
+
+    public PackageBuilder AddName(UName name)
+    {
+        if (package.Archive.NameIndices.ContainsKey(name.Index))
+        {
+            return this;
+        }
+
+        package.Names.Add(new UNameTableItem(name));
+        package.Archive.NameIndices[name.Index] = package.Names.Count - 1;
+
+        return this;
+    }
+
+    public PackageBuilder AddResources(params UObject[] objects)
+    {
+        foreach (var obj in objects)
+        {
+            AddResource(obj);
+        }
+
+        return this;
+    }
+
+    public PackageBuilder AddResource(UObject obj)
+    {
+        Contract.Assert(((UPackageIndex)obj).IsNull, "Cannot add an object that has already been indexed.");
+
+        if (obj.Package == package)
+        {
+            Contract.Assert(
+                obj.EnumerateOuter().Last(outer => outer == package.RootPackage) != null,
+                "Object must be part of the package's hierarchy."
+            );
+
+            var resource = new UExportTableItem(obj) { Object = obj };
+            package.Exports.Add(resource);
+            obj.PackageIndex = new UPackageIndex(package.Exports.Count);
+            obj.PackageResource = resource;
+            Debug.Assert(_Linker.IndexToObject(obj.PackageIndex) == obj);
+
+            // TODO: Figure this out.
+            package.Dependencies.Add([]);
+
+            package.Objects.Add(obj);
+
+        }
+        else
+        {
+            // TODO: Import all relevant resources as well!
+
+            var resource = new UImportTableItem(obj) { Object = obj };
+            package.Imports.Add(resource);
+            obj.PackageIndex = new UPackageIndex(-package.Imports.Count);
+            obj.PackageResource = resource;
+            Debug.Assert(_Linker.IndexToObject(obj.PackageIndex) == obj);
+
+            package.Objects.Add(obj);
+        }
+
+        return this;
+    }
+}
+
+public static class UnrealObjectBuilder
+{
+    public static UEnumBuilder CreateEnum(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("Enum");
+        return new UEnumBuilder(new UEnum
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UEnumBuilder Operate(UEnum obj)
+    {
+        return new UEnumBuilder(obj);
+    }
+
+    public static UStructBuilder CreateStruct(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("ScriptStruct") ?? package.FindObject<UClass>("Struct");
+        if (objectClass.Name == UnrealName.ScriptStruct)
+        {
+            return new UStructBuilder(new UScriptStruct
+            {
+                Package = package,
+                PackageIndex = UPackageIndex.Null,
+                Class = objectClass,
+                Outer = package.RootPackage
+            });
+        }
+
+        return new UStructBuilder(new UStruct
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UStructBuilder Operate(UStruct obj)
+    {
+        return new UStructBuilder(obj);
+    }
+
+    public static UObjectBuilder CreatePackage(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("Package");
+        return new UObjectBuilder(new UPackage
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UObjectBuilder Operate(UPackage obj)
+    {
+        return new UObjectBuilder(obj);
+    }
+
+    public static UFunctionBuilder CreateFunction(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("Function");
+        return new UFunctionBuilder(new UFunction
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UFunctionBuilder Operate(UFunction obj)
+    {
+        return new UFunctionBuilder(obj);
+    }
+
+    public static UStateBuilder CreateState(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("State");
+        return new UStateBuilder(new UState
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UStateBuilder Operate(UState obj)
+    {
+        return new UStateBuilder(obj);
+    }
+
+    public static UClassBuilder CreateClass(UnrealPackage package)
+    {
+        var objectClass = package.FindObject<UClass>("Class");
+        return new UClassBuilder(new UClass
+        {
+            Package = package,
+            PackageIndex = UPackageIndex.Null,
+            Class = objectClass,
+            Outer = package.RootPackage
+        });
+    }
+
+    public static UClassBuilder Operate(UClass obj)
+    {
+        return new UClassBuilder(obj);
+    }
+}
+
+public abstract class UFieldBuilder<TObject, TBuilder>(TObject obj) : UObjectBuilder<TObject, TBuilder>(obj)
+    where TObject : UField
+    where TBuilder : UFieldBuilder<TObject, TBuilder>
+{
+    public TBuilder NextField(UField field)
+    {
+        obj.NextField = field;
+
+        return (TBuilder)this;
+    }
+}
+
+public abstract class UEnumBuilder<TObject, TBuilder>(TObject obj) : UFieldBuilder<TObject, TBuilder>(obj)
+    where TObject : UEnum
+    where TBuilder : UEnumBuilder<TObject, TBuilder>
+{
+}
+
+public sealed class UEnumBuilder(UEnum obj) : UEnumBuilder<UEnum, UEnumBuilder>(obj);
+
+public abstract class UStructBuilder<TObject, TBuilder>(TObject obj) : UFieldBuilder<TObject, TBuilder>(obj)
+    where TObject : UStruct
+    where TBuilder : UStructBuilder<TObject, TBuilder>
+{
+    public TBuilder AddField(UStruct field)
+    {
+        obj.AddField(field);
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder RemoveField(UStruct field)
+    {
+        obj.RemoveField(field);
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder StructFlags(params StructFlag[] flagIndices)
+    {
+        obj.StructFlags = new UnrealFlags<StructFlag>(
+            obj.Package.Branch.EnumFlagsMap[typeof(StructFlag)],
+            flagIndices);
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder WithStatements(params UStruct.UByteCodeDecompiler.Token[] statements)
+    {
+        var script = new UByteCodeScript(obj, statements.ToList());
+        obj.Script = script;
+
+        return (TBuilder)this;
+    }
+}
+
+public sealed class UStructBuilder(UStruct obj) : UStructBuilder<UStruct, UStructBuilder>(obj);
+
+public abstract class UFunctionBuilder<TObject, TBuilder>(TObject obj) : UStructBuilder<TObject, TBuilder>(obj)
+    where TObject : UFunction
+    where TBuilder : UFunctionBuilder<TObject, TBuilder>
+{
+    public TBuilder FriendlyName(UName name)
+    {
+        if (obj.IsOperator())
+        {
+            var symbolMap = new Dictionary<char, string>
+            {
+                { ' ', "Spc" },
+                { '!', "Not" },
+                { '"', "DoubleQuote" },
+                { '#', "Pound" },
+                { '%', "Percent" },
+                { '&', "And" },
+                { '\'', "SingleQuote" },
+                { '(', "OpenParen" },
+                { ')', "CloseParen" },
+                { '*', "Multiply" },
+                { '+', "Add" },
+                { ',', "Comma" },
+                { '-', "Subtract" },
+                { '.', "Dot" },
+                { '/', "Divide" },
+                { ':', "Colon" },
+                { ';', "Semicolon" },
+                { '<', "Less" },
+                { '=', "Equal" },
+                { '>', "Greater" },
+                { '?', "Question" },
+                { '@', "At" },
+                { '[', "OpenBracket" },
+                { '\\', "Backslash" },
+                { ']', "CloseBracket" },
+                { '^', "Xor" },
+                { '_', "_" },
+                { '{', "OpenBrace" },
+                { '|', "Or" },
+                { '}', "CloseBrace" },
+                { '~', "Complement" }
+            };
+
+            string signature = "";
+            foreach (var c in name.ToString())
+            {
+                if (symbolMap.TryGetValue(c, out string newString))
+                {
+                    signature += newString;
+
+                    continue;
+                }
+
+                signature += c;
+            }
+
+            signature += "_";
+            if (obj.IsPre())
+            {
+                signature += "Pre";
+            }
+
+            foreach (var param in obj
+                .EnumerateFields<UProperty>()
+                .Where(property => property.IsParm() && !property.HasPropertyFlag(PropertyFlag.ReturnParm)))
+            {
+                signature += (param.Type) switch
+                {
+                    Types.PropertyType.ObjectProperty => ((UObjectProperty)param).Object.Name,
+                    Types.PropertyType.ClassProperty => ((UObjectProperty)param).Class.Name,
+                    Types.PropertyType.InterfaceProperty => ((UInterfaceProperty)param).InterfaceClass.Name,
+                    Types.PropertyType.StructProperty => ((UStructProperty)param).Struct.Name,
+                    _ => param.Name.ToString().Replace("Property", "")
+                };
+            }
+        }
+        else
+        {
+            obj.Name = name;
+        }
+
+        obj.FriendlyName = name;
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder FunctionFlags(params FunctionFlag[] flagIndices)
+    {
+        obj.FunctionFlags = new UnrealFlags<FunctionFlag>(
+            obj.Package.Branch.EnumFlagsMap[typeof(FunctionFlag)],
+            flagIndices);
+
+        return (TBuilder)this;
+    }
+}
+
+public sealed class UFunctionBuilder(UFunction obj) : UFunctionBuilder<UFunction, UFunctionBuilder>(obj);
+
+public abstract class UStateBuilder<TObject, TBuilder>(TObject obj) : UStructBuilder<TObject, TBuilder>(obj)
+    where TObject : UState
+    where TBuilder : UStateBuilder<TObject, TBuilder>
+{
+    public TBuilder StateFlags(params StateFlag[] flagIndices)
+    {
+        obj.StateFlags = new UnrealFlags<StateFlag>(
+            obj.Package.Branch.EnumFlagsMap[typeof(StateFlag)],
+            flagIndices);
+
+        return (TBuilder)this;
+    }
+}
+
+public sealed class UStateBuilder(UState obj) : UStateBuilder<UState, UStateBuilder>(obj);
+
+public abstract class UClassBuilder<TObject, TBuilder>(TObject obj) : UStateBuilder<TObject, TBuilder>(obj)
+    where TObject : UClass
+    where TBuilder : UClassBuilder<TObject, TBuilder>
+{
+    public TBuilder ClassFlags(params ClassFlag[] flagIndices)
+    {
+        obj.ClassFlags = new UnrealFlags<ClassFlag>(
+            obj.Package.Branch.EnumFlagsMap[typeof(ClassFlag)],
+            flagIndices);
+
+        return (TBuilder)this;
+    }
+}
+
+public sealed class UClassBuilder(UClass obj) : UClassBuilder<UClass, UClassBuilder>(obj);
+
+public abstract class UObjectBuilder<TObject, TBuilder>(TObject obj)
+    where TObject : UObject
+    where TBuilder : UObjectBuilder<TObject, TBuilder>
+{
+    public TObject Build()
+    {
+        return obj;
+    }
+
+    public TObject Build(out TObject outObj)
+    {
+        outObj = obj;
+
+        return outObj;
+    }
+
+    public TBuilder Name(UName name)
+    {
+        obj.Name = name;
+
+        if (obj is UStruct uStruct)
+        {
+            uStruct.FriendlyName = name; // Ensure that it is initialized, even when unwanted.
+        }
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder ObjectFlags(params ObjectFlag[] flagIndices)
+    {
+        obj.ObjectFlags = new UnrealFlags<ObjectFlag>(
+            obj.Package.Branch.EnumFlagsMap[typeof(ObjectFlag)],
+            flagIndices);
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder Class(UClass @class)
+    {
+        obj.Class = @class;
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder Outer(UObject? outer)
+    {
+        obj.Outer = outer;
+
+        return (TBuilder)this;
+    }
+
+    public TBuilder Archetype(UObject? archetype)
+    {
+        obj.Archetype = archetype;
+
+        return (TBuilder)this;
+    }
+}
+
+public sealed class UObjectBuilder(UObject obj) : UObjectBuilder<UObject, UObjectBuilder>(obj);
